0fc520ef2e6fd56178118ba0952991ea67430703
Marvellous Michaels <marvellousubani@gmail.com>
2019-12-30 11:30:54 +0100

    added draft and publish status for blog

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+
+  resources :blogs do 
+  	member do
+  		get :toggle_status
+  	end
+  end
+
   root to:'pages#home'
 end
